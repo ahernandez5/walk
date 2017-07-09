2c3f695a4c5374ff8099722d7aa239a063228b03
Aurora Hernandez <ahernandez38@csub.edu>
2017-07-09 13:56:57 -0700

    1. Finished the unit-test of vecNormalize() in walk.cpp.
    2. Wrote unit-test function in walk.cpp to test "#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]"
    
    3. Updated Makefile for -D UNIT_TEST

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ LFLAGS = -lrt -lX11 -lGLU -lGL -pthread -lm #-lXrandr
 
 all: walk
 
-walk: walk.cpp lab3http.cpp ppm.cpp log.cpp
+walk: walk.cpp lab3http.cpp ppm.cpp log.cpp 
 	g++ $(CFLAGS) $(LFLAGS) walk.cpp lab3http.cpp ppm.cpp log.cpp libggfonts.a -Wall -Wextra $(LFLAGS) -owalk -D UNIT_TEST
 	
 
--- walk.cpp
+++ walk.cpp
@@ -42,7 +42,7 @@ typedef Flt Matrix[4][4];
 #define rnd() (((double)rand())/(double)RAND_MAX)
 #define random(a) (rand()%a)
 #define MakeVector(x, y, z, v) (v)[0]=(x),(v)[1]=(y),(v)[2]=(z)
-#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
+#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2] 
 #define VecDot(a,b) ((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
 #define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
                       (c)[1]=(a)[1]-(b)[1]; \
